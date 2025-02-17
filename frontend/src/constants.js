4ecb3db3f7f3d41973a9a9c65b8ea74a8e7ba86c
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2025-02-17 11:09:21 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 // process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://my-e-shop.onrender.com';
 export const PRODUCTS_URL = '/api/v1/products';
 export const UPLOAD_URL = '/api/v1/upload';
 export const USERS_URL = '/api/v1/users';
